797121faf1dd59bcc9bfde7311c422163d796f52
kiarrobino <kyleiarrobino@gmail.com>
2026-04-05 00:51:44 -0500

    feat: implement models for type Dive and errors

---
+++ internal/model/errors.go
@@ -0,0 +1,12 @@
+package model
+
+import "errors"
+
+var (
+	ErrDiveNotFound      = errors.New("dive not found")
+	ErrInvalidDepth      = errors.New("depth must be greater than 0")
+	ErrInvalidDuration   = errors.New("duration must be greater than 0")
+	ErrInvalidVisibility = errors.New("visibility must be between 1 and 5")
+	ErrInvalidRating     = errors.New("rating must be between 1 and 5")
+	ErrInvalidDate       = errors.New("invalid date format, expected YYYY-MM-DD")
+)
